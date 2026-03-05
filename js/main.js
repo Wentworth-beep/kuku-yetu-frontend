3c9694b32211f0382dfd3dbdbe6eab88c7ef35c6
Wentworth-beep <santasantol087@gmail.com>
2026-03-05 15:10:35 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -231,6 +231,8 @@ function createProductCard(product) {
         stockClass = 'out';
         stockText = 'Out of stock';
     }
+     const placeholder = 'data:image/svg+xml,%3Csvg xmlns=\'http://www.w3.org/2000/svg\' viewBox=\'0 0 300 200\'%3E%3Crect width=\'300\' height=\'200\' fill=\'%23f0f0f0\'/%3E%3Ctext x=\'50%25\' y=\'50%25\' font-family=\'Arial\' font-size=\'16\' fill=\'%23999\' text-anchor=\'middle\' dy=\'.3em\'%3ELoading...%3C/text%3E%3C/svg%3E';
+    
     // Fix image URL - handle weird formatting
 let imagePath = product.images && product.images[0] ? product.images[0] : '';
 // Remove extra braces if they exist
